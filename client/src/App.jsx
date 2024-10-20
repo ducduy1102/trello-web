cfa73c90881a53dd4457dce72678f784171b7859
ducduy1102 <ducduy1110u@gmail.com>
2024-10-20 18:59:26 +0700

    Add MUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from "@mui/material/Button";
+import { pink } from "@mui/material/colors";
+import { Stack } from "@mui/material";
+import { AccessAlarm, ThreeDRotation, Home } from "@mui/icons-material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Stack direction="row" spacing={3}>
+        <Home />
+        <Home color="primary" />
+        <Home color="secondary" />
+        <Home color="success" />
+        <Home color="action" />
+        <Home color="disabled" />
+        <Home sx={{ color: pink[700] }} />
+      </Stack>
+      <br />
+      <AccessAlarm />
+      <br />
+      <ThreeDRotation />
+      <br />
+      <div className="">Evil shadow </div>
+      <br />
+      <Button variant="contained">Hello world</Button>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
