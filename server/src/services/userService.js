6dfe906c786bffb59bb9cab8a049d3cc314e193f
ducduy1102 <ducduy1110u@gmail.com>
2025-05-20 21:32:33 +0700

    Feat: api logout, refresh token

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -134,8 +134,33 @@ const login = async (reqBody) => {
   }
 };
 
+const refreshToken = async (clientRefreshToken) => {
+  try {
+    const refreshTokenDecoded = await JwtProvider.verifyToken(
+      clientRefreshToken,
+      env.REFRESH_TOKEN_SECRET_SIGNATURE
+    );
+
+    const userInfo = {
+      _id: refreshTokenDecoded._id,
+      email: refreshTokenDecoded.email,
+    };
+
+    const accessToken = await JwtProvider.generateToken(
+      userInfo,
+      env.ACCESS_TOKEN_SECRET_SIGNATURE,
+      env.ACCESS_TOKEN_LIFE
+    );
+
+    return { accessToken };
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const userService = {
   createNew,
   verifyAccount,
   login,
+  refreshToken,
 };
